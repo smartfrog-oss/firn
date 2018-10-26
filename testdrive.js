fe9775931e57c492bc97fb6e2b7378a93716631e
mouafa <mouafa.ahmed@smartfrog.com>
2018-10-26 17:16:10 +0200

    feat: write the diff image to disk

--- testdrive.js
+++ testdrive.js
@@ -2,7 +2,8 @@
 const firn = require('./main.js')
 const log = require('./src/lib/log')
 
-const urls = ['https://smartfrog.com/de-de/shop', 'https://smartfrog.com/de-de/shop/products']
+// const urls = ['https://smartfrog.com/de-de/shop', 'https://smartfrog.com/de-de/shop/products']
+const urls = ['https://smartfrog.com/de-de/shop']
 const config = { screenshotExt: 'png' }
 ;(async () => {
   const [err, raport] = await firn(urls, config)
